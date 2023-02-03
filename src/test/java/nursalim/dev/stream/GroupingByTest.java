a9f207c831c372de76b81c5980d099a252063076
nursalimdev <nursalimdev123@gmail.com>
2023-02-03 14:11:16 +0700

    Create GroupingByTest.java

---
+++ src/test/java/nursalim/dev/stream/GroupingByTest.java
@@ -0,0 +1,35 @@
+package nursalim.dev.stream;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class GroupingByTest {
+
+    @Test
+    void testGroupingBy(){
+        Map<String, List<Integer>> collect = Stream.of(1, 2, 3, 4, 5, 6, 7, 8)
+                .collect(Collectors.groupingBy(number -> number > 5 ? "Besar" : "Kecil"));
+
+        System.out.println(collect);
+    }
+
+    @Test
+    void testGroupingBy2(){
+        Map<String, List<String>> collect = Stream.of("Nursalim", "AL Farizi", "Edi", "Nani", "Indriyani")
+                .collect(Collectors.groupingBy(name -> name.length() > 4 ? "Panjang" : "Pendek"));
+        System.out.println(collect);
+
+    }
+
+    @Test
+    void testPartitioningBy(){
+        Map<Boolean, List<Integer>> collect = Stream.of(1, 2, 3, 4, 5, 6, 7, 8)
+                .collect(Collectors.groupingBy(number -> number > 5));
+
+        System.out.println(collect);
+    }
+}
